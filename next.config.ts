65f29bd2091fe741f8f26bcdaaa2efa0f865b3b5
Mohamed-Hema <mohamedhema2017@gmail.com>
2025-06-12 13:40:52 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No custom webpack config needed for JSON imports
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/el7a2ny_website",
+  assetPrefix: "/el7a2ny_website",
 };
 
 module.exports = nextConfig;
